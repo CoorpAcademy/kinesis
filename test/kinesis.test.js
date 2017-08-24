e6fbd3ccd5fb5e481201e3ea02a404f6627717c1
Adriean Khisbe <adriean.khisbe@live.fr>
2017-08-24 15:40:19 +0200

    setup test kinesis with kinesalite

--- test/kinesis.test.js
+++ test/kinesis.test.js
@@ -1,6 +1,14 @@
 import test from 'ava';
 import kinesis from '..';
 import {Duplex} from 'stream';
+import kinesalite from 'kinesalite';
+
+let kinesisServer;
+
+test.cb.before('set up the kinesalite', t => {
+  kinesisServer = kinesalite({ssl: true});
+  kinesisServer.listen(5678, t.end)
+})
 
 test('I can create a stream with a name', t => {
   const stream = kinesis.stream('name');
@@ -9,6 +17,14 @@ test('I can create a stream with a name', t => {
   t.deepEqual(stream.name, 'name')
 });
 
+test.cb('I can list stream from a kinesis', t => {
+  kinesis.listStreams({host: 'localhost', port: '5678'}, (err, streams) => {
+    t.is(err, null);
+    t.deepEqual(streams, [])
+    t.end()
+  })
+
+})
 
 test.todo('I can write to a kinesis stream')
 test.todo('I can read to a kinesis stream')
