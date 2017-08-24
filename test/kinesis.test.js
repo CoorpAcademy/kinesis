88c0170caa3c52550b1396f3f636564798dcd509
Adriean Khisbe <adriean.khisbe@live.fr>
2017-08-24 17:18:31 +0200

    write to kinesis test

--- test/kinesis.test.js
+++ test/kinesis.test.js
@@ -9,11 +9,15 @@ test.cb.beforeEach('setup kinesalite', t => {
   const port = ports++;
   const kinesisServer = kinesalite({ssl: true});
   const kinesisOptions = {host: 'localhost', port};
+  const kinesisStreamOptions = Object.assign({}, kinesisOptions, {name: 'a'})
   kinesisServer.listen(kinesisOptions.port, err => {
     if(err) return t.end(err);
-    kinesis.request('CreateStream', {StreamName: 'test', ShardCount: 2}, kinesisOptions, t.end)
+    kinesis.request('CreateStream', {StreamName: 'a', ShardCount: 2}, kinesisOptions, err => {
+      setTimeout(() => t.end(err), 1000);
+      // delai to ensure creation of stream
+    })
   });
-  t.context = {kinesisServer, kinesisOptions};
+  t.context = {kinesisServer, kinesisOptions, kinesisStreamOptions};
 });
 
 test.cb.afterEach('destroy the kinesalite', t => {
@@ -27,5 +31,14 @@ test('I can create a stream with a name', t => {
   t.deepEqual(stream.name, 'name')
 });
 
-test.todo('I can write to a kinesis stream')
-test.todo('I can read to a kinesis stream')
+test.cb('I can write to a kinesis stream', t => {
+  const kinesisStream = kinesis.stream(t.context.kinesisStreamOptions);
+  kinesisStream.write({PartitionKey: '12', Data: new Buffer('Hello Stream')}, null, t.end)
+})
+test.skip('I can read to a kinesis stream', t => {
+
+  //const kinesisStream = kinesis.stream(t.context.kinesisStreamOptions);
+ // kinesisStream.pipe(process.stdout)
+
+})
+test.todo('I can pipe streams')
