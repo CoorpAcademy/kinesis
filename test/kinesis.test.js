c6678d5d8e93291afacf86168af352069a420bd2
Adriean Khisbe <adriean.khisbe@live.fr>
2017-08-24 17:41:57 +0200

    add a test capturing the logic of reading the kinesis stream

--- test/kinesis.test.js
+++ test/kinesis.test.js
@@ -1,6 +1,6 @@
 import test from 'ava';
 import kinesis from '..';
-import {Duplex} from 'stream';
+import {Duplex, Writable} from 'stream';
 import kinesalite from 'kinesalite';
 
 let ports = 4657;
@@ -9,10 +9,10 @@ test.cb.beforeEach('setup kinesalite', t => {
   const port = ports++;
   const kinesisServer = kinesalite({ssl: true});
   const kinesisOptions = {host: 'localhost', port};
-  const kinesisStreamOptions = Object.assign({}, kinesisOptions, {name: 'a'})
+  const kinesisStreamOptions = Object.assign({}, kinesisOptions, {name: 'test'})
   kinesisServer.listen(kinesisOptions.port, err => {
     if(err) return t.end(err);
-    kinesis.request('CreateStream', {StreamName: 'a', ShardCount: 2}, kinesisOptions, err => {
+    kinesis.request('CreateStream', {StreamName: 'test', ShardCount: 2}, kinesisOptions, err => {
       setTimeout(() => t.end(err), 1000);
       // delai to ensure creation of stream
     })
@@ -35,10 +35,22 @@ test.cb('I can write to a kinesis stream', t => {
   const kinesisStream = kinesis.stream(t.context.kinesisStreamOptions);
   kinesisStream.write({PartitionKey: '12', Data: new Buffer('Hello Stream')}, null, t.end)
 })
-test.skip('I can read to a kinesis stream', t => {
 
-  //const kinesisStream = kinesis.stream(t.context.kinesisStreamOptions);
- // kinesisStream.pipe(process.stdout)
+test.cb('I can read from a kinesis stream', t => {
+  const writeableKinesisStream = kinesis.stream(t.context.kinesisStreamOptions);
+  const kinesisStream = kinesis.stream(t.context.kinesisStreamOptions);
 
+  var confirmRead = new Writable({objectMode: true})
+  confirmRead._write = function(chunk, encoding, cb) {
+    t.deepEqual(chunk.PartitionKey, '12')
+    t.end()
+    cb()
+  }
+  writeableKinesisStream.write({PartitionKey: '12', Data: new Buffer('Hello Stream')});
+  writeableKinesisStream.write({PartitionKey: '12', Data: new Buffer('Hello Stream')});
+  // note: normaly one should be enought but for strange reason probably linked to concurrency
+  // two are needed to trigger the write
+  kinesisStream.pipe(confirmRead)
 })
-test.todo('I can pipe streams')
+
+// FIXME: there seems to be problem with writeConcurrency
