50382c3dffce2a3fe5f7e7d8b99a92ab9e337a18
Adriean Khisbe <adriean.khisbe@live.fr>
2017-08-24 15:00:05 +0200

    hello stream test

--- test/kinesis.test.js
+++ test/kinesis.test.js
@@ -1,5 +1,10 @@
 import test from 'ava';
+import kinesis from '..';
+import {Duplex} from 'stream';
 
-test(t => {
-  t.deepEqual('ki' + 'nesis', 'kinesis');
+test('I can create a stream with a name', t => {
+  const stream = kinesis.stream('name');
+
+  t.true(stream instanceof Duplex);
+  t.deepEqual(stream.name, 'name')
 });
