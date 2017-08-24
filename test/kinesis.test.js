75ee7103c26c0bb214a6c48268b0e7a9ddf92bed
Adriean Khisbe <adriean.khisbe@live.fr>
2017-08-24 15:43:19 +0200

    create stream in the kinesis

--- test/kinesis.test.js
+++ test/kinesis.test.js
@@ -4,6 +4,7 @@ import {Duplex} from 'stream';
 import kinesalite from 'kinesalite';
 
 let kinesisServer;
+const kinesisOptions = {host: 'localhost', port: '5678'};
 
 test.cb.before('set up the kinesalite', t => {
   kinesisServer = kinesalite({ssl: true});
@@ -18,12 +19,22 @@ test('I can create a stream with a name', t => {
 });
 
 test.cb('I can list stream from a kinesis', t => {
-  kinesis.listStreams({host: 'localhost', port: '5678'}, (err, streams) => {
+  kinesis.listStreams(kinesisOptions, (err, streams) => {
     t.is(err, null);
     t.deepEqual(streams, [])
     t.end()
   })
+})
 
+test.cb('I can list create a stream in the kinesis', t => {
+  kinesis.request('CreateStream', {StreamName: 'test', ShardCount: 2}, kinesisOptions, err => {
+    t.is(err, null);
+    kinesis.listStreams(kinesisOptions, (err, streams) => {
+      t.is(err, null);
+      t.deepEqual(streams, ['test'])
+      t.end()
+    })
+  })
 })
 
 test.todo('I can write to a kinesis stream')
