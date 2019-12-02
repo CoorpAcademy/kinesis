317b9f73746d12d17845603f8583fb3b95ba8508
AdrieanKhisbe <adriean.khisbe@live.fr>
2019-12-02 15:23:13 +0100

    Move lib to src folder :file_cabinet:

--- kinesis.js
+++ kinesis.js
@@ -1,6 +1,6 @@
-const {request} = require('./lib/kinesis-request');
-const {listStreams} = require('./lib/kinesis-api');
-const KinesisStream = require('./lib/kinesis-stream');
+const {request} = require('./src/kinesis-request');
+const {listStreams} = require('./src/kinesis-api');
+const KinesisStream = require('./src/kinesis-stream');
 
 module.exports.stream = options => new KinesisStream(options);
 module.exports.KinesisStream = KinesisStream;
